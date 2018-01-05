7b92dee204fb82d0c287ca7b906cbc2e763b15e7
Damien Daco <dacodamien@gmail.com>
2018-01-05 20:48:46 +0100

    Added save list of IPs to .txt file feature

--- subnet_calculator.py
+++ subnet_calculator.py
@@ -10,6 +10,7 @@ from ui.design_subnet_calculator import Ui_MainWindow
 from app.logic import *
 import sys
 
+
 # TODO : Add feature to save ip list as .txt
 # TODO : Add multithreading. App is freezing when displaying large ranges
 
@@ -22,9 +23,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
 
         self.logic = Logic()
+        self.file_name = None
 
         self.shortcut_enter = QShortcut(QKeySequence("Enter"), self)
         self.shortcut_enter.activated.connect(self.process_ip)
+        self.action_save_as.triggered.connect(self.save_file)
 
     def process_ip(self):
 
@@ -50,6 +53,18 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         else:
             pass
+
+    def save_file(self):
+
+        if self.file_name is None:                                                   # Check if file name exists, if not, open save dialog
+            self.file_name = QFileDialog.getSaveFileName(self, 'Save File ', '.')[0] # This function returns a tuple, so let's extract the file patch with [0]
+
+        if not self.file_name.endswith('.txt'):                                      # If no extension specified, add .txt
+            self.file_name += '.txt'
+
+        with open(self.file_name, 'a') as file:
+            for i in self.logic.ip_list:
+                file.write('%s\n' % i)
 
 
 if __name__ == "__main__":
